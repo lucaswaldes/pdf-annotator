2e8014e26ccae9e7e348344cb1709e00993b3d4a
lucaswaldes <84790911+lucaswaldes@users.noreply.github.com>
2024-05-06 08:38:28 -0300

    feat: first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath === './') {
+        filePath = './demo/index.html';
+    }
+
+    const extname = String(path.extname(filePath)).toLowerCase();
+    const contentType = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+    }[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                res.writeHead(404);
+                res.end('404 Not Found');
+            } else {
+                res.writeHead(500);
+                res.end('500 Internal Server Error');
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
